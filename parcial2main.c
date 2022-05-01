1d9557debf76464269565b27e1fe946a4c187e7a
Sigfredo Olmedo <sigfredoolmedo6c17@gmail.com>
2022-05-01 01:04:56 -0500

    Delete TRISCbits.TRISC2

--- parcial2main.c
+++ parcial2main.c
@@ -7,132 +7,15 @@
 #include "lib4550.h"
 #define _XTAL_FREQ 4000000
 
-
-char a = 48;
-char b = 48;
-char c = 48;
-char d = 48;
-
 int contadorA = 1;
 int contadorB = 1;
-
-int variable = 0;
-
-void printNumber(int data) {
-
-    a = 48;
-    b = 48;
-    c = 48;
-    d = 48;
-
-    for (data = data; data > 999; data = data - 1000) d++;
-    for (data = data; data > 99; data = data - 100) c++;
-    for (data = data; data > 9; data = data - 10) b++;
-    for (data = data; data > 0; data = data - 1) a++;
-}
-
-void programa1() {
-    serialout_st("Temperatura en grados C:");
-    __delay_ms(2000);
-    int temperatura = 0;
-    int contador;
-    while (1) {
-        temperatura = get_temp();
-        printNumber(temperatura);
-        serialout(12);
-        serialout(c);
-        serialout(b);
-        __delay_ms(500);
-        if (temperatura > 312) {
-            serialout(12);
-            serialout_st("Temperatura excedida: ALERTA");
-            for (contador = 0; contador < 5; contador++) {
-                LED_BUILTIN = 1;
-                __delay_us(200);
-                LED_BUILTIN = 0;
-                __delay_us(200);
-                LED_BUILTIN = 1;
-                __delay_us(200);
-                LED_BUILTIN = 0;
-                __delay_us(200);
-                LED_BUILTIN = 1;
-                __delay_us(200);
-
-                if (contador == 5) {
-                    contador == 100;
-                    LED_BUILTIN = 0;
-                }
-            }
-        } else {
-            LED_BUILTIN = 0;
-        }
-    }
-    __delay_ms(500);
-
-}
-
-void programa2() {
-    serialout_st("Nivel de luz:");
-    __delay_ms(2000);
-    int luz = 0;
-    while (1) {
-        LED_BUILTIN = 0;
-        luz = get_lightlevel();
-        printNumber(luz);
-        serialout(12);
-        serialout(d);
-        serialout(c);
-        serialout(b);
-        serialout(a);
-        __delay_ms(500);
-        if (luz > 504) {
-            LED_BUILTIN = 1;
-        } else {
-            LED_BUILTIN = 0;
-        }
-        __delay_ms(500);
-    }
-}
-
-void programa3() {
-    serialout_st("Analog read en voltaje:");
-    __delay_ms(2000);
-    int nivel = 0;
-    while (1) {
-        nivel = analogRead(_AN2);
-        nivel = nivel / 2.44;
-        printNumber(nivel);
-        serialout(12);
-        serialout(c);
-        serialout_st(".");
-        serialout(b);
-        serialout(a);
-        serialout_st("V");
-        __delay_ms(500);
-    }
-}
 
 void main() {
     system_inicializacion();
     serialinit();
 
-        TRISCbits.TRISC2 = 0; //RC2 como salida
-        
-        BUZZER_CONF = OUTPUT;
-        BUZZER_WRITE = 0;
-
-    //    if (buttonA && buttonB) programa3();
-    //    if (buttonA) programa1();
-    //    if (buttonB) programa2();
-
-
-    //    CCPR1L = 0b00111110;
-    //    CCP1CONbits.DC1B1 = 1;
-    //    CCP1CONbits.DC1B0 = 0;
-    //    
-    //    int numero = 125;
-    //    CCPR2L = numero >> 2;
-    //    CCP2CONbits.DC2B = numero & 0b11;
+//    BUZZER_CONF = OUTPUT;
+//    BUZZER_WRITE = 0;
 
     if (buttonA);
     if (buttonB);
@@ -140,19 +23,6 @@ void main() {
 
     while (1) {
 
-        //        for (int i = 0; i <= 170; i++) {
-        //            switch (i) {
-        //                case 85: analogWrite(_PC1, 85);
-        //                    __delay_ms(500);
-        //                    break;
-        //                case 128: analogWrite(_PC1, 128);
-        //                    __delay_ms(500);
-        //                    break;
-        //                case 170: analogWrite(_PC1, 170);
-        //                    __delay_ms(500);
-        //                    break;
-        //            }
-        //        }
     }
 
 }
@@ -175,11 +45,6 @@ void ONbuttonA_pressed() {
         int velocidad3 = 254;
         buzzer(2000, 100);
 
-
-        //        if (variable <= 254) {
-        //            variable += 15;
-        //            analogWrite(_PC1, variable);
-        //        }
         switch (contadorA) {
             case 1: analogWrite(_PC2, velocidad1);
                 contadorA++;
@@ -236,17 +101,6 @@ void ONbuttonB_pressed() {
         buttonBflag = 1;
         buttonBpressedcounter = 0;
         buttonBpressedcounter2 = 0;
-        //your code here
-        //        if (variable >= 15) {
-        //            variable -= 15;
-        //            analogWrite(_PC1, variable);
-        //        }
-
-//        contadorB++
-//        if (contadorB == 4) {
-//            contadorB = 0;
-//        }
-
 
         switch (contadorB) {
             case 1:
@@ -275,14 +129,6 @@ void ONbuttonB_pressed() {
                 analogWrite(_PC2, 1);
                 contadorA = 1;
                 break;
-                //            case 3: analogWrite(_PC2, velocidad3);
-                //                contadorA++;
-                //                PORTB_WRITE = 0b01000000;
-                //                break;
-                //            case 4: analogWrite(_PC2, 1);
-                //                contadorA = 1;
-                //                PORTB_WRITE = 0b10000000;
-                //                break;
         }
 
 
